7b587cf481c089cdec2162ef2d9a2573f786be0b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-23 23:06:29 +0530

    deleteelem

--- day-2/server.js
+++ day-2/server.js
@@ -6,6 +6,22 @@ app.listen(3000,()=>{
     console.log("the server is running on 3000");
 })
 
+const post = []
+
+app.use(express.json())
+
 app.post("/notes",(req,res)=>{
-    
+    res.send("notes is created")
+
+    console.log(req.body);
+
+    post.push(req.body)
 })
+
+app.get("/notes",(req,res)=>{
+    res.send(post)
+})
+
+app
+
+
