2c753354b04bcec913318c6e6bc7bbcc71a58e9b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-23 20:12:20 +0530

    server

--- day-2/server.js
+++ day-2/server.js
@@ -2,14 +2,16 @@ const express = require("express")
 
 const app = express()
 
-app.use(express.json())
+const 
 
 app.post("/notes",(req,res)=>{
-    res.send("notes created")
-    console.log(req.body); 
+    res.send("notes is created")
+
+    console.log(req.body);
+    
+
 })
 
 app.listen(3000,()=>{
-    console.log("the server is runnig on 3000");
-    
+    console.log("the server is running on the 3000");
 })
