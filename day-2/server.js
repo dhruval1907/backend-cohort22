953c9c5ceb469066d6ed46da21d2e853e28869af
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-23 12:07:30 +0530

    makingserver

--- day-2/server.js
+++ day-2/server.js
@@ -2,18 +2,13 @@ const express = require("express")
 
 const app = express()
 
-app.get("/",(req,res)=>{
-    res.send("this is a home page")
+app.post("/notes",(req,res)=>{
+    res.send("notes created")
+    console.log(req.body);
+    
 })
 
-app.get("/about",(req,res)=>{
-    res.send("this is a about page ")
-})
-
-app.get("/home",(req,res)=>{
-    res.send("this is a home page")
-})
 app.listen(3000,()=>{
-    console.log("the server is working on the 3000");
+    console.log("the server is runnig on 3000");
     
 })
