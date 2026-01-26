22c9daa156cccaf8df88b8fe64af98c4cb9dfe14
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-26 10:06:06 +0530

    expressapi

--- day-2/server.js
+++ day-2/server.js
@@ -1,30 +1,11 @@
 const express = require("express")
-
 const app = express()
-
 app.listen(3000, () => {
-    console.log("the server is running on 3000");
+    console.log("the server is runnig on 3000");
 })
-
-const post = []
-
-app.use(express.json())
-
-app.post("/notes", (req, res) => {
+const notes = []
+app.post("/notes",(req,res)=>{
     res.send("notes is created")
-
-    console.log(req.body);
-
-    post.push(req.body)
+    
 })
-
-app.get("/notes", (req, res) => {
-    res.send(post)
-})
-
-app.delete("/notes/:index", (req, res) => {
-    delete post[req.params.index]
-    res.send('deleted successfully')
-})
-
 
