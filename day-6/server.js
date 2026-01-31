410cc3f30a7a370e22523a1f83e9f75e44f27036
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-31 15:00:23 +0530

    mongo

--- day-6/server.js
+++ day-6/server.js
@@ -1,10 +1,16 @@
 const app = require("./src/app")
+const mongoose = require("mongoose")
 
-function connection (){
-    
+// require("dotenv").config()
+
+function connection() {
+    mongoose.connect("mongodb+srv://Dhruval:6VtNOolnLqJBbUfB@cluster0.apwguvu.mongodb.net/day-2")
+    console.log("connected to DB");
+
 }
 
+connection()
 
-app.listen(3000,()=>{
-    console.log("server is running ont 3000");
+app.listen(3000, () => {
+    console.log("server is running on 3000");
 })
--- day-6/src/app.js
+++ day-6/src/app.js
@@ -1,5 +1,30 @@
 const express = require("express")
-
 const app = express()
+const notemodel = require("../models/notes.model")
 
-module.exports= app
+app.use(express.json())
+
+app.post("/notes", async (req, res) => {
+    const { title, description  } = req.body
+
+    const note = await notemodel.create({
+        title, description 
+    })
+
+    res.status(200).json({
+        message: "note is created",
+        note
+    })
+
+})
+
+app.get("/notes", async (req, res) => {
+    const notes = await notemodel.find()
+
+    res.status(200).json({
+        message: "fatching the notes",
+        notes,
+    })
+})
+
+module.exports = app
