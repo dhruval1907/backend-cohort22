5b89df6afcfb65c359db77a28ac01b61d2497315
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-02 01:17:55 +0530

    mongo connect

--- day-9/backend/config/database.js
+++ day-9/backend/config/database.js
@@ -5,6 +5,7 @@ function connectionDB() {
         .then(() => {
             console.log("Database is connected");
         })
+        .catch(err => console.log(err))
 }
 
 module.exports = connectionDB
--- day-9/backend/src/app.js
+++ day-9/backend/src/app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const noteModel = require("../models/data.model")
 
 const app = express()
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 
 app.post("/api/notes", async (req, res) => {
 
@@ -23,11 +25,31 @@ app.get("/api/notes", async (req, res) => {
     const note = await noteModel.find()
 
     res.status(200).json({
-        message : "fethcing the data",
+        message: "fethcing the data",
         note
     })
 })
 
-app.de
+app.delete("/api/notes/:indx", async (req, res) => {
+    const id = req.params.indx
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "note deleted",
+    })
+})
+
+app.patch("/api/notes/:indx", async (req, res) => {
+    const { description } = req.body
+    const id = req.params.indx
+
+    await noteModel.findByIdAndUpdate(id, { description })
+
+    res.status(200).json({
+        message: "data is modified"
+    })
+
+
+})
 
 module.exports = app
