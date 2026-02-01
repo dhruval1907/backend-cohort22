925ca72b65cb0eb4c9eac7c0292d4b1e7a139e99
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-02 01:29:11 +0530

    full completed

--- day-9/frontend/src/App.jsx
+++ day-9/frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import axios from "axios";
+import { useState } from 'react';
 const App = () => {
+
+  const [notes, setnotes] = useState([])
+  axios.get("http://localhost:3000/api/notes")
+    .then((res) => {
+      setnotes(res.data.note)
+    })
   return (
-    <div>App</div>
+    <div className='notes'>
+      {notes.map(function (note, idx) {
+        return <div key={idx}>
+          <div className="note">
+            <h1>{note.title}</h1>
+            <p>{note.description}</p>
+          </div>
+        </div>
+      })}
+    </div>
   )
 }
 
