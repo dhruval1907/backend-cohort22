86476e3c3d6f83c82c4a81e887bf5c73219fb888
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-02 00:49:07 +0530

    backend

--- day-9/backend/src/app.js
+++ day-9/backend/src/app.js
@@ -1,9 +1,31 @@
 const express = require("express")
+const noteModel = require("../models/data.model")
 
 const app = express()
 
-app.post("/api/notes",(req,res)=>{
-    
+app.use(express.json())
+
+app.post("/api/notes", async (req, res) => {
+
+    const { title, description } = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "notes is created",
+        note
+    })
+})
+
+app.get("/api/notes", async (req, res) => {
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message : "fethcing the data",
+        note
+    })
 })
 
 module.exports = app
