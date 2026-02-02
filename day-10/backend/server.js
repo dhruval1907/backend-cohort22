40d219da7ca0055a0b1f466b9df0288c0f088600
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-02 18:55:27 +0530

    full connect

--- day-10/backend/server.js
+++ day-10/backend/server.js
@@ -1,4 +1,8 @@
 const app = require("./src/app")
+const mongoDB  = require("./config/database")
+
+require("dotenv").config()
+mongoDB()
 
 app.listen(3000, () => {
     console.log("server is running on 3000");
--- day-10/backend/src/app.js
+++ day-10/backend/src/app.js
@@ -1,5 +1,41 @@
 const express = require("express")
+const noteModel = require("../models/data.models")
 
 const app = express()
+app.use(express.json())
+
+app.post("/api/notes", async (req, res) => {
+    const { title, description } = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "notes is created",
+        note
+    })
+})
+
+app.get("/api/notes", async (req, res) => {
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message: "fethcing the data",
+        note
+    })
+
+})
+
+app.delete("/api/notes/:indx", async (req, res) => {
+    const id = req.params.indx
+
+    
+
+})
+
+
+
+
 
 module.exports = app
