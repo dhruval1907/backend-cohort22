fac55e5ab64d7a1c45421b9a8166e631313d36d1
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-05 23:45:20 +0530

    enough for day

--- day-12/frontend/src/App.jsx
+++ day-12/frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
 const App = () => {
-  axios.get("http://localhost:3000/register")
-  .then((res)=>{
-    console.log(res.data);
+
+  const [users, setusers] = useState([])
+  function fethcingData() {
+    axios.get("http://localhost:3000/register")
+      .then((res) => {
+        setusers(res.data.users);
+      })
+  }
+  useEffect(function () {
+    fethcingData()
+  }, [])
+
+  axios.get("http://localhost:3000/register", {
+    const {email}
   })
+
+
   return (
     <div className='h-screen w-full bg-black'>
 
