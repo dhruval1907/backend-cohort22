ef7557e2bb850d654627bc55566b0b55b2fb4c38
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-05 23:27:48 +0530

    backend done

--- day-12/backend/src/app.js
+++ day-12/backend/src/app.js
@@ -5,7 +5,37 @@ const userModel = require("./models/user.model")
 app.use(express.json())
 
 app.post("/register", async (req, res) => {
+    const { email, password, name } = req.body
+    const users = await userModel.create({
+        email, name, password
+    })
+    res.status(201).json({
+        mesage: 'user created successfully',
+        users
+    })
+})
+app.get("/register", async (req, res) => {
+    const users = await userModel.find()
+    res.status(200).json({
+        message: "fethcing the users",
+        users
+    })
+})
+app.delete("/register/:id", async (req, res) => {
+    const id = req.params.id
+    await userModel.findByIdAndDelete(id)
 
+    res.status(200).json({
+        message: "user deleted",
+    })
+})
+app.patch("/register/:id", async (req, res) => {
+    const id = req.params.id
+    const {email} = req.body
+    await userModel.findByIdAndUpdate(id, { email })
+    res.status(200).json({
+        message: "user modified",
+    })
 })
 
 module.exports = app
