281c21619b1ebfb06f3bb049a5616dee8a2c511b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-06 10:45:05 +0530

    auth

--- day-11/index.js
+++ day-11/index.js
@@ -1,14 +1,19 @@
-const express = require("express")
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.set("view-engine", "ejs")
-
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) => {
-    res.render("index")
+    let token = jwt.sign({ email: "dhruvaldaladi1907@gmail.com" }, "secret")
+    res.cookie("token", token)
+    res.send("complete")
 })
-
-app.listen(3000, (req, res) => {
-    console.log("server is running on 3000");
+app.get("/read",(req,res)=>{
+    let data = jwt.sign(req.cookies.token,"secret")
+    console.log(data);
+    
 })
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
