9c0b0323abe9972809ef383b2ecba38b7d13356f
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-07 17:31:20 +0530

    register api done

--- day-12/backend/src/routes/auth.route.js
+++ day-12/backend/src/routes/auth.route.js
@@ -1,18 +1,13 @@
-require("dotenv").config()
 const express = require("express")
 const userModel = require("../models/user.model")
 const authRoute = express.Router()
 const jwt = require("jsonwebtoken")
-
-
 authRoute.post("/register", async (req, res) => {
     const { email, password, name } = req.body
-
-    const userAlreadyExists = await userModel.findOne({ email })
-
-    if (userAlreadyExists) {
+    const userAlreayExist = await userModel.findOne({ email })
+    if (userAlreayExist) {
         return res.status(400).json({
-            message: 'user already exists'
+            message: "user is already exists"
         })
     }
 
@@ -20,13 +15,12 @@ authRoute.post("/register", async (req, res) => {
         email, name, password
     })
 
-    const token = jwt.sign({
+    let token = jwt.sign({
         id: users._id,
+        name: users.name,
         email: users.email,
         password: users.password
-    },
-        process.env.JWT_SECRET
-    )
+    }, process.env.JWT_SECRET)
 
     res.cookie("token", token)
 
@@ -37,4 +31,12 @@ authRoute.post("/register", async (req, res) => {
     })
 })
 
+authRoute.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    
+
+})
 module.exports = authRoute
