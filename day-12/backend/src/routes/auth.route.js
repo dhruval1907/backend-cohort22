2eda459d2dcd127393c4f58ea349dfcf20c1224d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-07 18:45:52 +0530

    auth registration

--- day-12/backend/src/routes/auth.route.js
+++ day-12/backend/src/routes/auth.route.js
@@ -2,71 +2,101 @@ const express = require("express")
 const userModel = require("../models/user.model")
 const authRoute = express.Router()
 const jwt = require("jsonwebtoken")
-const crypto = require("crypto");
-
 authRoute.post("/register", async (req, res) => {
     const { email, password, name } = req.body
-    const userAlreayExist = await userModel.findOne({ email })
-    if (userAlreayExist) {
-        return res.status(400).json({
-            message: "user is already exists"
+    const isEmailMatched = await userModel.findOne({ email })
+    if (isEmailMatched) {
+        return res.status(409).json({
+            message: "email already exists"
         })
     }
-
-    const hash = crypto.createHash("md5").update(password).digest("hex")
-
-    console.log(hash);
-
     const users = await userModel.create({
-        email, name, password: hash
+        name, password, email
     })
-
-    let token = jwt.sign({
+    const token = jwt.sign({
         id: users._id,
         name: users.name,
         email: users.email,
-        password: users.password
+        passowrd: users.password,
     }, process.env.JWT_SECRET)
-
-    res.cookie("token", token)
-
     res.status(201).json({
-        mesage: 'user created successfully',
+        message: "user created",
         users,
         token
     })
 })
 
-authRoute.post("/login", async (req, res) => {
-    const { email, password } = req.body
-
-    const user = await userModel.findOne({ email })
-
-    if (!user) {
-        return res.status(404).json({
-            message: "user note found"
-        })
-    }
-
-    const isPasswordmatched = user.password === crypto.createHash("md5").update(password).digest("hex")
-
-    if (!isPasswordmatched) {
-        return res.send(401).json({
-            message: "invalid password"
-        })
-    }
-
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET)
 
 
-    res.cookie("token", token)
 
-    res.status(201).json({
-        mesage: 'user logged in      successfully',
-        user,
-        token
-    })
-})
+
+
+
+
+
+
+
+// authRoute.post("/register", async (req, res) => {
+//     const { email, password, name } = req.body
+//     const userAlreayExist = await userModel.findOne({ email })
+
+//     if (userAlreayExist) {
+//         return res.status(400).json({
+//             message: "user is already exists"
+//         })
+//     }
+
+//     const hash = crypto.createHash("md5").update(password).digest("hex")
+//     const users = await userModel.create({
+//         email, name, password: hash
+//     })
+
+//     let token = jwt.sign({
+//         id: users._id,
+//         name: users.name,
+//         email: users.email,
+//         password: users.password
+//     }, process.env.JWT_SECRET)
+
+//     res.cookie("token", token)
+
+//     res.status(201).json({
+//         mesage: 'user created successfully',
+//         users,
+//         token
+//     })
+// })
+
+// authRoute.post("/login", async (req, res) => {
+//     const { email, password } = req.body
+
+//     const user = await userModel.findOne({ email })
+
+//     if (!user) {
+//         return res.status(404).json({
+//             message: "user note found"
+//         })
+//     }
+
+//     const isPasswordmatched = user.password === crypto.createHash("md5").update(password).digest("hex")
+
+//     if (!isPasswordmatched) {
+//         return res.send(401).json({
+//             message: "invalid password"
+//         })
+//     }
+
+//     const token = jwt.sign({
+//         id: user._id
+//     }, process.env.JWT_SECRET)
+
+
+//     res.cookie("token", token)
+
+//     res.status(201).json({
+//         mesage: 'user logged in      successfully',
+//         user,
+//         token
+//     })
+// })
 module.exports = authRoute
