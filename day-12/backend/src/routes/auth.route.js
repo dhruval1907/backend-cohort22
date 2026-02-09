baef78de10636fe27038d6a0c716d2d4f61c209c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-09 11:24:16 +0530

    login done

--- day-12/backend/src/routes/auth.route.js
+++ day-12/backend/src/routes/auth.route.js
@@ -3,6 +3,7 @@ const authRouter = express.Router()
 const userModel = require("../models/user.model")
 const cookie = require("cookie-parser")
 const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
 authRouter.post("/register", async (req, res) => {
     const { email, password, name } = req.body
 
@@ -14,8 +15,14 @@ authRouter.post("/register", async (req, res) => {
         })
     }
 
+    const userPass = crypto.createHash("md5").update(password).digest("hex")
+
+
+
     const user = await userModel.create({
-        email, password, name
+        email,
+        password: userPass
+        , name
     })
 
     const token = jwt.sign({
@@ -37,7 +44,29 @@ authRouter.post("/register", async (req, res) => {
 
 authRouter.post("/login", async (req, res) => {
     const { email, password } = req.body
-    const user = await 
+    const user = await userModel.findOne({ email })
+    if (!user) {
+        return res.status(400).json({
+            message: "user not found"
+        })
+    }
+    const userPassword = user.password === crypto.createHash("md5").update(password).digest("hex")
+    if (!userPassword) {
+        return res.status(400).json({
+            message: "password invalid"
+        })
+    }
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+    res.status(200).json({
+        message: "user logged in",
+        user,
+        token
+    })
 })
 
 
