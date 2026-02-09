ed0d248989c7a1328a12d4aef4d19485d85b9da2
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-09 18:51:50 +0530

    login  backend completed

--- day-12/backend/src/routes/auth.route.js
+++ day-12/backend/src/routes/auth.route.js
@@ -6,40 +6,30 @@ const jwt = require("jsonwebtoken")
 const crypto = require("crypto")
 authRouter.post("/register", async (req, res) => {
     const { email, password, name } = req.body
-
     const isAlreadyExist = await userModel.findOne({ email })
-
     if (isAlreadyExist) {
         return res.status(409).json({
             message: "user already registered"
         })
     }
-
     const userPass = crypto.createHash("md5").update(password).digest("hex")
-
-
-
     const user = await userModel.create({
         email,
         password: userPass
         , name
     })
-
     const token = jwt.sign({
         name: user.name,
         id: user._id,
         email: user.email,
         password: user.password
     }, process.env.JWT_SECRET)
-
     res.cookie("token", token)
-
     res.status(201).json({
         message: "user registered",
         user,
         token
     })
-
 })
 
 authRouter.post("/login", async (req, res) => {
@@ -47,26 +37,20 @@ authRouter.post("/login", async (req, res) => {
     const user = await userModel.findOne({ email })
     if (!user) {
         return res.status(400).json({
-            message: "user not found"
+            message: "user invalid"
         })
     }
-    const userPassword = user.password === crypto.createHash("md5").update(password).digest("hex")
-    if (!userPassword) {
+    const isPasswordMatched = user.password === password
+    if (!isPasswordMatched) {
         return res.status(400).json({
-            message: "password invalid"
+            message: "password is incorrect"
         })
     }
-    const token = jwt.sign({
-        id: user._id,
-        email: user.email
-    }, process.env.JWT_SECRET)
-
-    res.cookie("token", token)
     res.status(200).json({
         message: "user logged in",
-        user,
-        token
+        user
     })
+
 })
 
 
